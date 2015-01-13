be007ddd42697c0a07e79c044941c763a4512266
Logan Garber <LoganGarber@hotmail.com>
2015-01-12 22:21:55 -0500

    getting git setup with source tree process steps: 1. create an eclipse project (you might want to keep the project names the same to avoid confusion) 2. empty that file of everything eclipse just created 3. create source tree repository as that now empty file 4. open eclipse and your file should be right there for you to use whenever you want 5. just make sure that you checkout the latest file whenever you start working on it again

--- src/initial/initial.java
+++ src/initial/initial.java
@@ -1,5 +1,10 @@
 package initial;
 
 public class initial {
-
+	public static void main(String [] args){
+		for (int counter = 0; counter < 10;counter ++)
+		{
+			System.out.println("hello");
+		} 
+	}
 }
