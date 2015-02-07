a4ec6ca4d2cd5b48b2ea16d79c39d18a6cf33894
Logan Garber <LoganGarber@hotmail.com>
2015-02-07 01:05:08 -0500

    more things

--- src/initial/initial.java
+++ src/initial/initial.java
@@ -4,7 +4,7 @@ public class initial {
 	public static void main(String [] args){
 		for (int counter = 0; counter < 10;counter ++)
 		{
-			System.out.println("Hello " + "World");
+			System.out.println("HelloWorld");
 		} 
 	}
 }
